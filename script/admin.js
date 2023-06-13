ec4f11d0ef8711c27c5439a2e24828153fc45a8f
Masood-B <masoodbasardien123@gmail.com>
2023-06-13 15:40:11 +0200

    yeah add work, still doing edit

--- script/admin.js
+++ script/admin.js
@@ -42,76 +42,76 @@ const savePrice = document.querySelector("#input2"); // Price input
 const savePicture = document.querySelector("#input3"); // Picture input
 const saveDescription = document.querySelector("#input4"); // Description input
 const result = document.querySelector("#data") //display list
-let productList = JSON.parse(localStorage.getItem('product-list')) ? JSON.parse(localStorage.getItem('product-list')) : []; // info
-let productID = productList[productList.length-1] ? productList[productList.length-1].id + 1: 1; // ID number
+let products = JSON.parse(localStorage.getItem('product-list')); // info
+let productID = products[products.length-1] ? products[products.length-1].id + 1: 1; // ID number
 let deleteBtn; // Delete button
 let editBtn; // edit button
 
 // Admin Items
-let products = [
-    {
-    id: 1,
-    name:"Monster Musume no Iru Nichijou",
-    price:"R 399",
-    picture:"https://i.postimg.cc/Xqc6mRbC/Monster-Musume-volume-1-cover.png",
-    description: "aaaaaaaaaaaaaaaaaaaaaaaaaa",
-    date: new Date()
-    },
-    {
-    id: 2,
-    name: "No Game No Life",
-    price:"R 499",
-    picture:"https://i.postimg.cc/Wp6RNcBD/no-game-no-life-vol-1-2.jpg",
-    description: "fffffffffffffffffffffffffffff",
-    date: new Date()
-    },
-    {
-    id: 3,
-    name:"Yuragi-sou no Yuuna-san",
-    price:"R 299",
-    picture:"https://i.postimg.cc/TPm7trGy/sk1g6a44e3121.jpg",
-    description: "ddddddddddddddddddddddddddddddd",
-    date: new Date()
-    },
-    {
-    id: 4,
-    name:"Saikin Yatotta Maid ga Ayashii",
-    price:"R 399",
-    picture:"https://i.postimg.cc/2jTH9jpx/the-maid-i-hired-recently-is-mysterious-vol-2.jpg",
-    description: "ssssssssssssssssssssssssssssssss",
-    date: new Date()
-    },
-    {
-    id: 5,
-    name:"Mato Seihei no Slave",
-    price:"R 299",
-    picture:"https://i.postimg.cc/QxwJvHfc/Volume-01.webp",
-    description: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-    date: new Date()
-    }
-]
-localStorage.setItem("save-key", JSON.stringify(products));
-JSON.parse(localStorage.getItem("save-key"));
+// let products = [
+//     {
+//     id: 1,
+//     name:"Monster Musume no Iru Nichijou",
+//     price:"R 399",
+//     picture:"https://i.postimg.cc/Xqc6mRbC/Monster-Musume-volume-1-cover.png",
+//     description: "Snake girl and guy",
+//     date: new Date()
+//     },
+//     {
+//     id: 2,
+//     name: "No Game No Life",
+//     price:"R 499",
+//     picture:"https://i.postimg.cc/Wp6RNcBD/no-game-no-life-vol-1-2.jpg",
+//     description: "brother and sister",
+//     date: new Date()
+//     },
+//     {
+//     id: 3,
+//     name:"Yuragi-sou no Yuuna-san",
+//     price:"R 299",
+//     picture:"https://i.postimg.cc/TPm7trGy/sk1g6a44e3121.jpg",
+//     description: "cat girl and human girl",
+//     date: new Date()
+//     },
+//     {
+//     id: 4,
+//     name:"Saikin Yatotta Maid ga Ayashii",
+//     price:"R 399",
+//     picture:"https://i.postimg.cc/2jTH9jpx/the-maid-i-hired-recently-is-mysterious-vol-2.jpg",
+//     description: "maid",
+//     date: new Date()
+//     },
+//     {
+//     id: 5,
+//     name:"Mato Seihei no Slave",
+//     price:"R 299",
+//     picture:"https://i.postimg.cc/QxwJvHfc/Volume-01.webp",
+//     description: "school samurai girl",
+//     date: new Date()
+//     }
+// ]
+// localStorage.setItem("save-key", JSON.stringify(products));
+// JSON.parse(localStorage.getItem("save-key"));
 
 // add button
 addBtn.addEventListener("click", addItem);
 result.innerHTML = "";
-function addItem(){
+function addItem(event){
     event.preventDefault();
     if(saveName.value == "", savePrice.value == "", savePicture.value == "", saveDescription.value == ""){
         alert("You Forgot To Add!")
     }else{
         products.push({
-            id: productID,
+            id: products.length,
             name: saveName.value,
             price: savePrice.value ,
             picture: savePicture.value,
             description: saveDescription.value,
             date: new Date()
         });
-        productID++;
+        // productID++;
         saveName.value = "", savePrice.value = "", savePicture.value = "", saveDescription.value = "";
-        localStorage.setItem("product-list", JSON.stringify(productList));
+        localStorage.setItem("product-list", JSON.stringify(products));
         displayProduct();
     }
 }
@@ -120,11 +120,19 @@ function addItem(){
 function editItem(){
   editBtn = [...document.querySelectorAll("#edit-btn")];
   editBtn.forEach((item)=>{
-    item.addEventListener("click", editingProduct)
+    item.addEventListener("click", EditingProduct)
   })
 }
-function editingProduct(){
-  
+function EditingProduct(){
+  this.id = item.id;
+  this.name = document.querySelector("#nameInput").value;
+  this.price = document.querySelector("#priceInput").value;
+  this.picture = document.querySelector("#pictureInput").value;
+  this.description = document.querySelector("#descriptionInput").value;
+  let index = editBtn.indexOf(event,target);
+  products[index] = {id: this.id, name: this.name, price: this.price.name, description: this.description};
+  localStorage.setItem("product-list", JSON.stringify(products));
+  displayProduct();
 }
 
 // delete button
@@ -136,15 +144,15 @@ function deleteButton(){
 }
 function deleteItem(){
     let startPoint = deleteBtn.indexOf(event.target);
-    productList.splice(startPoint, 1);
-    localStorage.setItem("product-list", JSON.stringify(productList));
+    products.splice(startPoint, 1);
+    localStorage.setItem("product-list", JSON.stringify(products));
     displayProduct();
 }
 
 // Main Function
 function displayProduct(){
   result.innerHTML = "";
-products.forEach((stock)=>{
+  products.forEach((stock)=>{
   result.innerHTML += 
   `
   <tr>
@@ -157,26 +165,26 @@ products.forEach((stock)=>{
       </td>
       <td>${stock.description}</td>
       <td>
-      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editProduct${stock.id}">Edit</button>
+      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target='#${stock.id}'>Edit</button>
       <!-- Modal -->
-      <div class="modal fade" id="editProduct${stock.id}" tabindex="-1" aria-labelledby="editProductLabel${stock.id}" aria-hidden="true">
+      <div class="modal fade" id='${stock.id}' tabindex="-1" aria-labelledby="editProductLabel${stock.id}" aria-hidden="true">
         <div class="modal-dialog">
           <div class="modal-content">
             <div class="modal-header">
-              <h1 class="modal-title fs-5" id="editProductLabel${stock.id}">Modal title</h1>
+              <h1 class="modal-title fs-5" id="editProductLabel${stock.id}">Edit</h1>
               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
             <form>
             <div class="container">
               <p>Name</p>
-              <input type="text" class="form-control" placeholder="Enter Title..." id="input1" value="${stock.name}" aria-label="Title">
+              <input type="text" class="form-control" placeholder="Enter Title..." id="nameInput" value="${stock.name}" aria-label="Title" required>
               <p>Price</p>
-              <input type="text" class="form-control" placeholder="Enter Your Amount..." id="input2" value="${stock.price}" aria-label="Price">
+              <input type="text" class="form-control" placeholder="Enter Your Amount..." id="priceInput" value="${stock.price}" aria-label="Price" required>
               <p>Picture</p>
-              <input type="text" class="form-control" placeholder="Enter Image URL..." id="input3" value="${stock.picture}" aria-label="Picture">
+              <input type="text" class="form-control" placeholder="Enter Image URL..." id="pictureInput" value="${stock.picture}" aria-label="Picture" required>
               <p>Despcription</p>
-              <textarea class="form-control" placeholder="Enter Description..." id="input4" aria-label="Description">${stock.description}</textarea>
+              <textarea class="form-control" placeholder="Enter Description..." id="descriptionInput" aria-label="Description" required>${stock.description}</textarea>
               </div>
               </form>
             </div>
@@ -192,6 +200,14 @@ products.forEach((stock)=>{
   `
 })
 deleteButton();
+editItem();
 }
 
 displayProduct();
+
+function deleteItem(item) {
+  let index = products.findIndex( p=> p.id == item.id)
+  products.splice()
+  localStorage.setItem('', JSON.stringify(products))
+  displayProduct()
+}
---
+++ script/product.js
@@ -0,0 +1,71 @@
+let products = JSON.parse(localStorage.getItem('product-list')) ? 
+    JSON.parse(localStorage.getItem('product-list')) :
+    localStorage.setItem('product-list', JSON.stringify(
+        [
+        {
+        id: 1,
+        name:"Monster Musume no Iru Nichijou",
+        price: 399,
+        picture:"https://i.postimg.cc/Xqc6mRbC/Monster-Musume-volume-1-cover.png",
+        description: "Snake girl and guy",
+        date: new Date()
+        },
+        {
+        id: 2,
+        name: "No Game No Life",
+        price: 499,
+        picture:"https://i.postimg.cc/Wp6RNcBD/no-game-no-life-vol-1-2.jpg",
+        description: "brother and sister",
+        date: new Date()
+        },
+        {
+        id: 3,
+        name:"Yuragi-sou no Yuuna-san",
+        price: 299,
+        picture:"https://i.postimg.cc/TPm7trGy/sk1g6a44e3121.jpg",
+        description: "cat girl and human girl",
+        date: new Date()
+        },
+        {
+        id: 4,
+        name:"Saikin Yatotta Maid ga Ayashii",
+        price: 399,
+        picture:"https://i.postimg.cc/2jTH9jpx/the-maid-i-hired-recently-is-mysterious-vol-2.jpg",
+        description: "maid",
+        date: new Date()
+        },
+        {
+        id: 5,
+        name:"Mato Seihei no Slave",
+        price: 299,
+        picture:"https://i.postimg.cc/QxwJvHfc/Volume-01.webp",
+        description: "school samurai girl",
+        date: new Date()
+        }
+    ]
+    )) 
+
+let divBooks = document.querySelector(".books");
+try{
+    products.forEach((reader)=> {
+        divBooks.innerHTML += `
+        <div class="cards-wrapper">
+        <div>
+        <h2>${reader.name}</h2>
+        </div>
+        <div class= "card-image">
+        <img src="${reader.picture}" alt="${reader.name}" loading="lazy" class="img-fluid">
+        <div>
+        <p class="lead">${reader.description}</p>
+        </div>
+        <button class"btn">Add to cart</button>
+        </div>
+        <div>
+        <p>Price: R${reader.price}</p>
+        </div>
+        </div>
+        `
+    })
+}catch(e) {
+    location.reload()
+}
